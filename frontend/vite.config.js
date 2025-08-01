dec2948e6466cbe96f244549a48ced1c1a599efc
bfreccero20 <bautifreccero@gmail.com>
2025-07-31 22:48:29 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,1 +1,10 @@
-export default { root: './src' };
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  root: './src',
+  build: {
+    outDir: '../dist'
+  }
+})
